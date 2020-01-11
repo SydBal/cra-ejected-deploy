6525b38583608db2b90fad1e08af3a978af01514
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-11 00:10:48 +0000

    Deploy SydBal/cra-ejected-deploy to github.com/SydBal/cra-ejected-deploy.git:gh-pages

---
+++ precache-manifest.cda323b44d10ef361f67055accc85e5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa32f0cbaefe93be950077e74b6c655b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f39eb4124dbe98f34bfa",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8c90cf6d310890931729",
+    "url": "./static/js/2.72e42a66.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.72e42a66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f39eb4124dbe98f34bfa",
+    "url": "./static/js/main.79c031ad.chunk.js"
+  },
+  {
+    "revision": "505a2b3dc123a59a464d",
+    "url": "./static/js/runtime-main.b584a3bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
